955616592da2f4ea6789b5d56953fe0008f0fa56
smillingblade <oluwadarafaithful@gmail.com>
2026-04-28 15:52:24 +0100

    add TaskList component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,17 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  staticDirs: ["../public"],
+  stories: ['../src/components/**/*.stories.@(ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-vitest",
+    '@storybook/addon-docs',
+    '@storybook/addon-vitest',
+    '@chromatic-com/storybook',
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
 };
+
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,6 @@
-import type { Preview } from "@storybook/react-vite";
+import type { Preview } from '@storybook/react-vite';
+
+import '../src/index.css';
 
 const preview: Preview = {
   parameters: {
